769045a02c4f5771bfec44341113b46fae0eeca3
Eli Koederitz <bkoederitz@mail.bradley.edu>
2026-07-23 10:49:18 -0500

    Show Recommendations

--- GMTK 2026/Assets/Scripts/Penguins/Penguin.cs
+++ GMTK 2026/Assets/Scripts/Penguins/Penguin.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 /// <summary>
 /// Controls detecting clicks on penguins and showing the popup that gives ther recomendations.
 /// </summary>
-public class PenguinRecomendation : MonoBehaviour
+public class Penguin : MonoBehaviour
 {
     private Dictionary<RocketSection, RocketPart> recomendations;
+    private PenguinRecDisplay display;
 
     public Dictionary<RocketSection, RocketPart> Recommendations => recomendations;
 
-    public void Initialize(Dictionary<RocketSection, RocketPart> recs)
+    public void Initialize(Dictionary<RocketSection, RocketPart> recs, PenguinRecDisplay display)
     {
         recomendations = recs;
+        this.display = display;
+    }
+
+    /// <summary>
+    /// Show the popup when the penguin is clicked on.
+    /// </summary>
+    private void OnMouseDown()
+    {
+        display.ShowPenguin(this);
     }
 }
