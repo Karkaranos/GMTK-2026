3883717b74abbde6ec458ebb523776921c946a56
Cade Naylor <arnaylor@mail.bradley.edu>
2026-07-23 13:48:30 -0500

    temp push

---
+++ GMTK 2026/Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,32 @@
+/*****************************************
+ * Author Name:     Cade Naylor           
+ * Created Date:    7/23/2026
+ * Modified Date:   7/23/2026
+ * Description:     Stores all Event References
+ ******************************************/
+using UnityEngine;
+using FMODUnity;
+using NaughtyAttributes;
+
+public class FMODEvents : MonoBehaviour
+{
+    [SerializeField, Foldout("Music")] public EventReference MainMenu;
+
+    [SerializeField, Foldout("SFX")] public EventReference TestSound;
+
+
+    public static FMODEvents instance { get; private set; }
+
+    /// <summary>
+    /// Creates a single instance
+    /// </summary>
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
